490ae7020e3aafa72664fb765a1bcc6b7fd7fa20
lrxjason <lrxjason@gmail.com>
2018-04-04 21:31:33 -0500

    Add files via upload

--- BPNN_Hand_Writing_recognition.py
+++ BPNN_Hand_Writing_recognition.py
@@ -1,11 +1,8 @@
 import numpy as np
 import tensorflow as tf
-import matplotlib.pyplot as plt
 from tensorflow.examples.tutorials.mnist import input_data
-import os
 import cv2
 
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 def draw_circle(event, x, y, flags, param):
     global ix, iy, drawing, mode, img
@@ -15,7 +12,7 @@ def draw_circle(event, x, y, flags, param):
         ix, iy = x, y
 
     elif event == cv2.EVENT_MOUSEMOVE:
-        if drawing == True:
+        if drawing:
             cv2.circle(img, (x, y), 5, (255, 255, 255), -1)
 
     elif event == cv2.EVENT_LBUTTONUP:
@@ -24,6 +21,7 @@ def draw_circle(event, x, y, flags, param):
 
     elif event == cv2.EVENT_RBUTTONDOWN:
         img = np.zeros((140, 140, 3), np.uint8)
+
 
 # NETWORK TOPOLOGIES
 n_hidden_1 = 256
@@ -49,30 +47,31 @@ biases = {
 }
 print("NETWORK READY")
 
+
 def multilayer_perceptron(_X, _weights, _biases):
     layer_1 = tf.nn.sigmoid(tf.add(tf.matmul(_X, _weights['w1']), _biases['b1']))
     layer_2 = tf.nn.sigmoid(tf.add(tf.matmul(layer_1, _weights['w2']), _biases['b2']))
-    return (tf.matmul(layer_2, _weights['out']) + _biases['out'])
+    return tf.matmul(layer_2, _weights['out']) + _biases['out']
+
 
 # PREDICTION
 pred = multilayer_perceptron(x, weights, biases)
 
 # LOSS AND OPTIMIZER
-cost = tf.reduce_sum(np.square(pred - y))
+cost = tf.reduce_sum(np.square(tf.nn.softmax_cross_entropy_with_logits_v2(logits=pred, labels=y)))
 optm = tf.train.AdamOptimizer(learning_rate=0.001).minimize(cost)
 corr = tf.equal(tf.argmax(pred, 1), tf.argmax(y, 1))
 accr = tf.reduce_mean(tf.cast(corr, "float"))
 
 # INITIALIZER
 init = tf.global_variables_initializer()
-print ("FUNCTIONS READY")
+print("FUNCTIONS READY")
 
 saver = tf.train.Saver()
 
-
 training_epochs = 50
-batch_size      = 64
-display_step    = 10
+batch_size = 64
+display_step = 10
 
 do_train = 0
 # LAUNCH THE GRAPH
@@ -86,11 +85,11 @@ if do_train == 1:
     testimg = mnist.test.images
     testlabel = mnist.test.labels
     print("MNIST ready")
-    
+
     # OPTIMIZE
     for epoch in range(training_epochs):
         avg_cost = 0.
-        total_batch = int(mnist.train.num_examples/batch_size)
+        total_batch = int(mnist.train.num_examples / batch_size)
         # ITERATION
         for i in range(total_batch):
             batch_xs, batch_ys = mnist.train.next_batch(batch_size)
@@ -99,17 +98,17 @@ if do_train == 1:
             avg_cost += sess.run(cost, feed_dict=feeds)
         avg_cost = avg_cost / total_batch
         # DISPLAY
-        if (epoch+1) % display_step == 0:
-            print ("Epoch: %03d/%03d cost: %.9f" % (epoch, training_epochs, avg_cost))
+        if (epoch + 1) % display_step == 0:
+            print("Epoch: %03d/%03d cost: %.9f" % (epoch, training_epochs, avg_cost))
             feeds = {x: batch_xs, y: batch_ys}
             train_acc = sess.run(accr, feed_dict=feeds)
-            print ("TRAIN ACCURACY: %.3f" % (train_acc))
+            print("TRAIN ACCURACY: %.3f" % train_acc)
             feeds = {x: mnist.test.images, y: mnist.test.labels}
             test_acc = sess.run(accr, feed_dict=feeds)
-            print ("TEST ACCURACY: %.3f" % (test_acc))
-    
+            print("TEST ACCURACY: %.3f" % test_acc)
+
     saver_path = saver.save(sess, "save_BPNN_model/BPNN_model")
-    print ("OPTIMIZATION FINISHED")
+    print("OPTIMIZATION FINISHED")
 
 if do_train == 0:
     drawing = False
@@ -120,15 +119,15 @@ if do_train == 0:
     cv2.setMouseCallback('image', draw_circle)
     saver.restore(sess, "save_BPNN_model/BPNN_model")
     prediction = tf.argmax(pred, 1)
-    
-    while(1):
+
+    while 1:
         cv2.imshow('image', img)
         key = cv2.waitKey(1)
         if key == 97:
             print('Image saved')
             cv2.imwrite('aaa.jpg', img)
             img = np.zeros((140, 140, 3), np.uint8)
-            
+
             image = cv2.imread('aaa.jpg', 0)
             resized_image = cv2.resize(image, (28, 28))
             im = np.reshape(np.array(resized_image), (-1, 784))
--- CNN_Hand_Writing_Recognition.py
+++ CNN_Hand_Writing_Recognition.py
@@ -1,9 +1,7 @@
 import numpy as np
 import tensorflow as tf
-import matplotlib.pyplot as plt
 from tensorflow.examples.tutorials.mnist import input_data
 import cv2
-
 
 
 def draw_circle(event, x, y, flags, param):
@@ -14,7 +12,7 @@ def draw_circle(event, x, y, flags, param):
         ix, iy = x, y
 
     elif event == cv2.EVENT_MOUSEMOVE:
-        if drawing == True:
+        if drawing:
             cv2.circle(img, (x, y), 5, (255, 255, 255), -1)
 
     elif event == cv2.EVENT_LBUTTONUP:
@@ -24,20 +22,21 @@ def draw_circle(event, x, y, flags, param):
     elif event == cv2.EVENT_RBUTTONDOWN:
         img = np.zeros((140, 140, 3), np.uint8)
 
-n_input  = 784
+
+n_input = 784
 n_output = 10
-weights  = {
-        'wc1': tf.Variable(tf.random_normal([3, 3, 1, 64], stddev=0.1)),
-        'wc2': tf.Variable(tf.random_normal([3, 3, 64, 128], stddev=0.1)),
-        'wd1': tf.Variable(tf.random_normal([7*7*128, 1024], stddev=0.1)),
-        'wd2': tf.Variable(tf.random_normal([1024, n_output], stddev=0.1))
-    }
-biases   = {
-        'bc1': tf.Variable(tf.random_normal([64], stddev=0.1)),
-        'bc2': tf.Variable(tf.random_normal([128], stddev=0.1)),
-        'bd1': tf.Variable(tf.random_normal([1024], stddev=0.1)),
-        'bd2': tf.Variable(tf.random_normal([n_output], stddev=0.1))
-    }
+weights = {
+    'wc1': tf.Variable(tf.random_normal([3, 3, 1, 64], stddev=0.1)),
+    'wc2': tf.Variable(tf.random_normal([3, 3, 64, 128], stddev=0.1)),
+    'wd1': tf.Variable(tf.random_normal([7 * 7 * 128, 1024], stddev=0.1)),
+    'wd2': tf.Variable(tf.random_normal([1024, n_output], stddev=0.1))
+}
+biases = {
+    'bc1': tf.Variable(tf.random_normal([64], stddev=0.1)),
+    'bc2': tf.Variable(tf.random_normal([128], stddev=0.1)),
+    'bd1': tf.Variable(tf.random_normal([1024], stddev=0.1)),
+    'bd2': tf.Variable(tf.random_normal([n_output], stddev=0.1))
+}
 
 
 def conv_basic(_input, _w, _b, _keepratio):
@@ -70,6 +69,8 @@ def conv_basic(_input, _w, _b, _keepratio):
            'fc1': _fc1, 'fc_dr1': _fc_dr1, 'out': _out
            }
     return out
+
+
 print("CNN READY")
 
 x = tf.placeholder(tf.float32, [None, n_input])
@@ -79,7 +80,7 @@ keepratio = tf.placeholder(tf.float32)
 # FUNCTIONS
 
 _pred = conv_basic(x, weights, biases, keepratio)['out']
-cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=_pred, labels=y))
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=_pred, labels=y))
 optm = tf.train.AdamOptimizer(learning_rate=0.001).minimize(cost)
 _corr = tf.equal(tf.argmax(_pred, 1), tf.argmax(y, 1))
 accr = tf.reduce_mean(tf.cast(_corr, tf.float32))
@@ -109,7 +110,7 @@ if do_train == 1:
 
     for epoch in range(training_epochs):
         avg_cost = 0.
-        total_batch = int(mnist.train.num_examples/batch_size)
+        total_batch = int(mnist.train.num_examples / batch_size)
         # Loop over all batches
         for i in range(total_batch):
             batch_xs, batch_ys = mnist.train.next_batch(batch_size)
@@ -122,9 +123,9 @@ if do_train == 1:
         if epoch % display_step == 0:
             print("Epoch: %03d/%03d cost: %.9f" % (epoch, training_epochs, avg_cost))
             train_acc = sess.run(accr, feed_dict={x: batch_xs, y: batch_ys, keepratio: 1.})
-            print(" Training accuracy: %.3f" % (train_acc))
+            print(" Training accuracy: %.3f" % train_acc)
             test_acc = sess.run(accr, feed_dict={x: testimg, y: testlabel, keepratio: 1.})
-            print (" Test accuracy: %.3f" % (test_acc))
+            print(" Test accuracy: %.3f" % test_acc)
     # Save Net
     saver.save(sess, "save_CNN_model/cnn_mnist")
 
@@ -150,7 +151,7 @@ if do_train == 0:
     # test_acc = sess.run(accr, feed_dict={x: testimg, y: testlabel, keepratio: 1.})
     # print(" TEST ACCURACY: %.3f" % (test_acc))
 
-    while (1):
+    while 1:
         cv2.imshow('image', img)
         key = cv2.waitKey(1)
         if key == 97:
